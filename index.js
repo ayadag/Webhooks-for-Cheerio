3f2f923b5cb6b258a6d6ba72fca8b9feff6f0f0e
ayadag <109915071+ayadag@users.noreply.github.com>
2023-12-31 20:09:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-const express = require("require");
+const express = require("express");
+
 const axios = require("axios");
+
 const cheerio = require("cheerio");
+
+require('dotenv').config();
+
 const app = express();
+
 const port = process.env.PORT || 5000;
+
 const url = "https://en.wikipedia.org/wiki/U.S_state";
+
 
 let states = [];
 
+
 const fetchData = async () => {
+
 try {
+
 let res = await axios.get(url);
+
 let $ = await cheerio.load(res.data);
+
 $(
+
 "#mw-content-text > div.mw-parser-output > div.div-col"
+
 ).each((i, e) => {
+
 states.push($(e).text().trim());
+
 });
+
 } catch (e) {
+
 console.log(e);
+
 }
+
 };
+
 fetchData();
 
+
 app.get("/states", (req, res) => {
+
 res.send(states);
+
 });
+
 
 app.listen(port, () => console.log("server running"));
