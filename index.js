83783643b56fe37465fdae7a87389d227e71691a
ayadag <109915071+ayadag@users.noreply.github.com>
2024-01-01 18:56:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,29 @@
 const express = require("express");
-
 const axios = require("axios");
-
 const cheerio = require("cheerio");
-
 const app = express();
-
 const port = process.env.PORT || 5000;
-
-const url = "https://www.todaysessay.com/sitemap.xml";
-
+const url = "https://en.wikipedia.org/wiki/U.S_state";
 
 let states = [];
 
-
 const fetchData = async () => {
-
 try {
-
 let res = await axios.get(url);
-
 let $ = await cheerio.load(res.data);
-
 $(
-
-"pre"
-
+"a"
 ).each((i, e) => {
-
-states.push($(e).text().trim());
-
+states.push($(e).attr('href').trim());
 });
-
 } catch (e) {
-
 console.log(e);
-
 }
-
 };
-
 fetchData();
 
-
 app.get("/states", (req, res) => {
-
 res.send(states);
-
 });
-
 
 app.listen(port, () => console.log("server running"));
